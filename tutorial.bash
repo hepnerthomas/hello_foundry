336e743b765dd736e9bd07e5ff58e85e3fa9ea40
Thomas Hepner <hepner.thomas@gmail.com>
2022-11-15 17:05:23 -0800

    feat: add foundry commands to create NFT contract and mint new NFTs from the deployed contract on Goerhli.

--- tutorial.bash
+++ tutorial.bash
@@ -9,3 +9,9 @@ cast send --rpc-url=$RPC_URL 0x28c6Eb216bFC10F0fA27d0dC774217F90b7D7AbC  "mintTo
 
 ### Sanity check the owner of the NFT
 cast call --rpc-url=$RPC_URL --private-key=$WALLET_PRIVATE_KEY 0x28c6Eb216bFC10F0fA27d0dC774217F90b7D7AbC "ownerOf(uint256)" 1
+
+### Deploy NFT contract with additional metadata
+forge create NFT --rpc-url=$RPC_URL --private-key=$WALLET_PRIVATE_KEY --constructor-args TTHOMASTOKENNMR TH_NMR_TOKEN https://i.seadn.io/gae/Wi3m40tiBAleV7JCywJSwWEC29zc8IC2sKgCXjExNDDW4xUMamTPcY6099BzdAmHdNueNJNTVJv726KqA_S664YUXyjpEm5T4Etj?auto=format&w=1000
+
+### Mint NFTs from new contract
+cast send --rpc-url=$RPC_URL 0x9e2deb0b0ffe75002019ead8e0f040c5ed34c6b2  "mintTo(address)" 0xF241374eb31da7CE550A95bA1DacdFF27475d7cC --private-key=$WALLET_PRIVATE_KEY --value 0.01ether
